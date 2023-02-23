58ce33c4d28a22614cf5ec9c75abff165e722fb6
Brandon Trump <brandontrump101@gmail.com>
2023-02-23 13:45:18 +1300

    Created new routes for workouts

--- server/server.js
+++ server/server.js
@@ -2,18 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 
+const workoutRoutes = require("./routes/workouts");
+
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
