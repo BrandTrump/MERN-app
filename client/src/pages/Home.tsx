eb38e1d938dc8a0acefd51d81a978b64adc91861
Brandon Trump <brandontrump101@gmail.com>
2023-02-27 14:33:43 +1300

    Added useEffect hook to retrieve all workouts from server

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/workout-details/WorkoutDetails";
 
 const Home = () => {
+  interface workoutProps {
+    title: string;
+    reps: number;
+    load: number;
+    createdAt: string;
+  }
+
+  const [workouts, setWorkouts] = useState<workoutProps[]>([]);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:4000/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) setWorkouts(json);
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="workouts">
+        {workouts.map((workout: workoutProps, id) => {
+          return (
+            <div>
+              <WorkoutDetails
+                key={id}
+                title={workout.title}
+                reps={workout.reps}
+                load={workout.load}
+                createdAt={workout.createdAt}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
