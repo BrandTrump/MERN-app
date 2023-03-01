2bb400525858a71d354287d071a46a384823830c
Brandon Trump <brandontrump101@gmail.com>
2023-03-01 18:56:08 +1300

    Added react context & reducer

--- client/src/components/forms/WorkoutForm.tsx
+++ client/src/components/forms/WorkoutForm.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useWorkoutsContext } from "../../hooks/useWorkoutContext";
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
   const [title, setTitle] = useState<string>("");
   const [load, setLoad] = useState<string>("");
   const [reps, setReps] = useState<string>("");
@@ -30,6 +32,7 @@ const WorkoutForm = () => {
       setReps("");
       setError(null);
       console.log("new workout added", json);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
   return (
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import WorkoutForm from "../components/forms/WorkoutForm";
 import WorkoutDetails from "../components/workout-details/WorkoutDetails";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
 const Home = () => {
   interface workoutProps {
@@ -10,14 +11,16 @@ const Home = () => {
     createdAt: string;
   }
 
-  const [workouts, setWorkouts] = useState<workoutProps[]>([]);
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("http://localhost:4000/api/workouts");
       const json = await response.json();
 
-      if (response.ok) setWorkouts(json);
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
     };
 
     fetchWorkouts();
@@ -26,18 +29,19 @@ const Home = () => {
   return (
     <div className="home">
       <div className="workouts">
-        {workouts.map((workout: workoutProps, id) => {
-          return (
-            <div key={id}>
-              <WorkoutDetails
-                title={workout.title}
-                reps={workout.reps}
-                load={workout.load}
-                createdAt={workout.createdAt}
-              />
-            </div>
-          );
-        })}
+        {workouts &&
+          workouts.map((workout: workoutProps, id: any) => {
+            return (
+              <div key={id}>
+                <WorkoutDetails
+                  title={workout.title}
+                  reps={workout.reps}
+                  load={workout.load}
+                  createdAt={workout.createdAt}
+                />
+              </div>
+            );
+          })}
       </div>
       <WorkoutForm />
     </div>
