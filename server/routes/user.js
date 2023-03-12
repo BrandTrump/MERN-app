43e34458d68423c2ced5d84fbceedd4a3556995d
Brandon Trump <brandontrump101@gmail.com>
2023-03-12 22:31:51 +1300

    fixed route

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { loginUser, signupUser } = require("../controllers/userController");
 const router = express.Router();
 
-router.post("/login", () => loginUser);
+router.post("/login", loginUser);
 
-router.post("/signup", () => signupUser);
+router.post("/signup", signupUser);
 
 module.exports = router;
