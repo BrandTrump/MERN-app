739b420ac20eaa185cf9cba5f80c09a9ad8e04f3
Brandon Trump <brandontrump101@gmail.com>
2023-03-13 14:09:21 +1300

    created authentication context & hook

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const AuthContext = createContext({} as any);
+
+export const authReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT":
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }: Props) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  console.log("AuthContext state: ", state);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
