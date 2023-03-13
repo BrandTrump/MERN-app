d70d4c71c4d56feb78498f7d1270119b111daa15
Brandon Trump <brandontrump101@gmail.com>
2023-03-13 14:10:27 +1300

    wrapped the auth context provider around the entire app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
 import { WorkoutsContextProvider } from "./context/WorkoutContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
