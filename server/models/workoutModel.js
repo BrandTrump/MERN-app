1684f811800fceb4e5cac6fae006a9731e62f1ed
Brandon Trump <brandontrump101@gmail.com>
2023-03-14 15:49:29 +1300

    added user id to the model

--- server/models/workoutModel.js
+++ server/models/workoutModel.js
@@ -16,6 +16,10 @@ const workoutShema = new Schema(
       type: Number,
       required: true,
     },
+    user_id: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
